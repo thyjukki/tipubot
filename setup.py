a879ca159a0e0aa3199e819969a5dbed061286dd
Jussi Joki <judejoki@gmail.com>
2021-04-23 14:46:09 +0300

    setup base project with sample bot

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tipubot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='jukki',
+    author_email='contact@jukk.it',
+    description='Telegram bot for TIPU'
+)
